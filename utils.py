8e670e5828bd08b9432d4f52242600085c4e3712
marcoabrate <marcopietro.abrate@gmail.com>
2025-06-25 16:49:42 +0100

    part 1 and 2 done, part 3 evaluate_rnn ready

--- utils.py
+++ utils.py
@@ -1,111 +1,64 @@
 import torch
-
+import numpy as np
  
-class RNNModule(torch.nn.Module):
-    def __init__(self, n_inputs, n_hidden, nonlinearity, device, input_bias=True, hidden_bias=True):
-        super(RNNModule, self).__init__()
-
-        self.rnn_cell = RNNCell(n_inputs, n_hidden, nonlinearity, input_bias, hidden_bias)
-        self.n_hidden = n_hidden
-
-        self.device = device
-
-    def forward(self, x, hidden=None):
-        # x: [BATCH SIZE, TIME, N_FEATURES]
-        
-        output = torch.zeros(x.shape[0], x.shape[1], self.n_hidden).to(self.device)
-
-        if hidden is None:
-            h_out = torch.zeros(x.shape[0], self.n_hidden) # initialize hidden state
-            h_out = h_out.to(self.device)
-        else:
-            h_out = hidden
-
-        window_size = x.shape[1]
-
-        # loop over time
-        for t in range(window_size):
-            x_t = x[:,t,...]
-            h_out = self.rnn_cell(x_t, h_out)
-            output[:,t,...] = h_out
-
-        # return all outputs, and the last hidden state
-        return output, h_out
-
-class RNNCell(torch.nn.Module):
-    def __init__(self, n_inputs, n_hidden, nonlinearity, input_bias, hidden_bias):
-        super(RNNCell, self).__init__()
-
-        if nonlinearity == 'sigmoid':
-            activation_fn = torch.sigmoid
-        else:
-            print("[!!!] WARNING: activation function not recognized, using identity")
-            activation_fn = torch.nn.Identity()
-
-        self.in2hidden = torch.nn.Linear(n_inputs, n_hidden, bias=input_bias)
-        self.hidden2hidden = torch.nn.Linear(n_hidden, n_hidden, bias=hidden_bias)
-
-        self.activation_fn = activation_fn
-
-    def forward(self, x, hidden):
-        igates = self.in2hidden(x)
-        hgates = self.hidden2hidden(hidden)
-        return self.activation_fn(igates + hgates)
-
-def test_rnn_epoch(rnn, dataloader, loss_fn, stepwise=True, device=torch.device('cuda')):
+def evaluate_rnn(
+    device,
+    rnn,
+    dataloader,
+    loss_fn,
+    for_ratemaps
+):
     '''
     Test the RNN for one epoch on the given dataloader.
     Args:
+        device: the device to use for computation
         rnn: the RNN model to test
         dataloader: the dataloader containing the test data
         loss_fn: the loss function to use for evaluation
-        stepwise: if True, evaluate the RNN step by step, otherwise evaluate the whole sequence at once.
-                  if False, hidden states and other information will only be recorded for the last time step
-        device: the device to use for computation (default: cuda)
+        for_ratemaps: ...
     Returns:
-        test_dict: a dictionary containing the test results, including losses, hidden states, positions,
-                     head directions, angles, outputs, and output labels
+        d: a dictionary containing the test results, including losses, hidden states, positions,
+            head directions, angles, outputs, and output labels
     '''
     rnn.eval()
-    test_dict = {'batch_losses':[], 'h_ts':[], 'pos':[], 'hds':[], 'thetas':[], 'outputs':[], 'outputs_label':[]}
+
+    d = {
+        'batch_losses': [],
+        'hidden_states':[],
+        'positions':[],
+        'head_directions':[],
+        'outputs':[],
+        'embs_labels':[]
+    }
+
     with torch.no_grad():
-        h_t = None
-        for i, data in enumerate(dataloader):
-            embs, vels, rot_vels, pos, hds, thetas, embs_labels = data
+        hidden_state = None # Initialize hidden state to zeros
+        for i, batch in enumerate(dataloader):
+            embs, vels, rot_vels, pos, hds, embs_labels = batch
             
-            inputs = torch.cat((embs.squeeze(dim=0).to(device), vels.squeeze(dim=0)[:,0,...].to(device), rot_vels.squeeze(dim=0)[:,0,...].to(device)), dim=-1)
-            if stepwise:
-                for step in range(inputs.shape[1]):
-                    outputs, h_t = rnn.forward(inputs[:, step, ...][:,None,:], hidden=h_t)   # index at step 0, keep dim
+            inputs = torch.cat((
+                embs.squeeze(dim=0).to(device),
+                vels.squeeze(dim=0).to(device),
+                rot_vels.squeeze(dim=0).to(device)
+            ), dim=-1)
 
-                    labels = embs_labels.squeeze(dim=0)[:,:,step,...].to(device)
-                    assert labels.shape == outputs.shape, f"Labels shape {labels.shape} and outputs shape {outputs.shape} do not match"
-                    loss = loss_fn(outputs, labels)
+            hidden_all_new = rnn.inputs2hidden(inputs, hidden_state)
+            hidden_new = hidden_all_new[:, -1, :]
+            outputs = rnn.hidden2outputs(hidden_all_new)
 
-                    test_dict['loss'].append(loss.item())
-                    
-                    test_dict['h_ts'].append(h_t.detach().cpu().numpy())                           # (fps, hidden_dim)
-                    test_dict['pos'].append(pos.detach()[0,:,0,step,...].cpu().numpy())                # (fps, 2)
-                    test_dict['hds'].append(hds.detach()[0,:,0,step,...].cpu().numpy())                # (fps, 2)
-                    test_dict['thetas'].append(thetas.detach()[0,:,0,step,...].cpu().numpy())          # (fps, 1)
-                    test_dict['outputs'].append(outputs.detach().cpu().numpy())            # (fps, emb_dim)
-                    test_dict['outputs_label'].append(labels.detach().cpu().numpy())
-            else:
-                outputs, h_t = rnn.forward(inputs, hidden=h_t)
-                
-                labels = embs_labels.squeeze(dim=(0,2)).to(device)
-                assert labels.shape[0] == outputs.shape[0], f"Labels shape {labels.shape} and outputs shape {outputs.shape} do not match"
-                loss = loss_fn(outputs, labels)
+            embs_labels = embs_labels.squeeze(dim=0).to(device)
+            batch_losses = loss_fn(outputs, embs_labels)
 
-                test_dict['loss'].append(loss.item())
-                
-                test_dict['h_ts'].append(h_t.detach().cpu().numpy())                           # (fps, hidden_dim)
-                test_dict['pos'].append(pos.detach()[0,:,0,-1,:].cpu().numpy())                # (fps, 2)
-                test_dict['hds'].append(hds.detach()[0,:,0,-1,:].cpu().numpy())                # (fps, 2)
-                test_dict['thetas'].append(thetas.detach()[0,:,0,-1,:].cpu().numpy())          # (fps, 1)
-                test_dict['outputs'].append(outputs.detach()[:,-1,:].cpu().numpy())            # (fps, emb_dim)
-                test_dict['outputs_label'].append(labels.detach()[:,-1,:].cpu().numpy())
-                
-        test_dict['loss_avg'] = np.mean(test_dict['loss'])
-    
-    return test_dict
+            d['batch_losses'].append(batch_losses.detach().item())
+            
+            if for_ratemaps:
+                d['hidden_states'].append(hidden_all_new.detach().cpu().numpy())                           # (fps, hidden_dim)
+                d['positions'].append(pos.squeeze(dim=0).detach().cpu().numpy())                # (fps, 2)
+                d['head_directions'].append(hds.squeeze(dim=0).detach().cpu().numpy())                # (fps, 2)
+                d['outputs'].append(outputs.detach().cpu().numpy())            # (fps, emb_dim)
+                d['embs_labels'].append(embs_labels.detach().cpu().numpy())
+    if for_ratemaps:
+        for k in ['hidden_states', 'positions', 'head_directions', 'outputs', 'embs_labels']:
+            d[k] = np.concatenate(d[k], axis=1)
+
+    return d
